ec229e9db68c7c42c5dfb922015639783bcf9943
Andrew Eldridge <andrew-eldridge@codermerlin.codermerlin.com>
2022-09-14 13:07:22 -0500

    made the function actually executable with STDIN and STDOUT

--- sortListAlphabetically.py
+++ sortListAlphabetically.py
@@ -1,25 +1,35 @@
+#!/usr/bin/env python3
+
 import sys
 
-words = open test.txt, a+r
-wordList.write(words.read())
+wordsList = []
 
-for line in wordList:
-    wordList.append(line.replace("\n", "").lower())
-    
+words = open(r"test.txt","r")
+
+for line in words:
+    wordsList.append(line)
+
+wordsList = [x[:-1] for x in wordsList]
+
+print(wordsList) 
+
 def partition(low, high, arr):
-    pivot, part = ord(arr[high[0]]), low
+    pivot, part = ord(arr[high][0]), low
     for j in range(low, high):
-        if ord(arr[j[0]]) <= pivot:
+        if ord(arr[j][0]) <= pivot:
             arr[j], arr[part] = arr[part], arr[j]
             part += 1
     arr[part], arr[high] = arr[high], arr[part]
     return part
 
-def quicksort(low, high, arr):
+def quicksort(low, high, arr, index):
     if len(arr) == 1:
-        return arr
+        return arr[index]
     if low < high:
         pi = partition(low, high, arr)
-        quicksort(low, pi - 1, arr)
-        quicksort(pi + 1, high, arr)
-    return arr
+        quicksort(low, pi - 1, arr, index)
+        quicksort(pi + 1, high, arr, index)
+    return arr[index]
+
+for index in range(len(wordsList) - 1):
+    print(quicksort(0, len(wordsList) - 1, wordsList, index))
