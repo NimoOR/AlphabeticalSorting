e601b7f863c1e5be576ff09f131f8fedf2d47a4b
Andrew Eldridge <andrew-eldridge@codermerlin.codermerlin.com>
2022-09-15 20:13:33 -0500

    fixed some of the sorting

--- sortListAlphabetically.py
+++ sortListAlphabetically.py
@@ -6,16 +6,22 @@ words = sys.stdin.read()
 wordsList = []
 
 wordsList = words.split("\n")
-
 '''print(words)'''
 print(wordsList)
 
 def partition(low, high, arr):
     pivot, part = ord(arr[high][0].lower()), low
     for j in range(low, high):
-        if ord(arr[j][0].lower()) <= pivot:
+        if ord(arr[j][0].lower()) < pivot:
             arr[j], arr[part] = arr[part], arr[j]
             part += 1
+        if ord(arr[j][0].lower()) == pivot:
+            char = 0
+            while ord(arr[j][char].lower()) == ord(arr[high][char]):
+                char += 1
+            if ord(arr[j][char].lower()) < ord(arr[high][char]):
+                arr[j], arr[part] = arr[part], arr[j]
+                part += 1
     arr[part], arr[high] = arr[high], arr[part]
     return part
 
