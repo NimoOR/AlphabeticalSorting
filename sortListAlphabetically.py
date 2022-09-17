c095a538fab1fed047813fce7706a53a5eb9a9b9
Andrew Eldridge <andrew-eldridge@codermerlin.codermerlin.com>
2022-09-17 00:09:58 -0500

    optimized variables

--- sortListAlphabetically.py
+++ sortListAlphabetically.py
@@ -5,11 +5,10 @@ sys.setrecursionlimit(1500)
 
 words = sys.stdin.read()
 wordsList = words.split("\n")
-#print(wordsList)
 wordsList.pop()
+
 def partition(low, high, arr):
-    pivot = arr[high].lower()
-    part = low - 1
+    pivot, part = arr[high].lower(), low - 1
     for j in range(low, high):
         if arr[j].lower() < pivot:
             part += 1
@@ -25,7 +24,7 @@ def quicksort(low, high, arr):
         quicksort(low, pi - 1, arr)
         quicksort(pi + 1, high, arr)    
     return arr
+
 final = quicksort(0, len(wordsList) - 1, wordsList)
 for index in range(len(wordsList) - 1):
-    print(final[index])
-    
+    print(final[index])    
