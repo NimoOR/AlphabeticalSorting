b95f6715489e827499bf853f1533d172fca131a4
Andrew Eldridge <andrew-eldridge@codermerlin.codermerlin.com>
2022-09-16 23:53:39 -0500

    pushing better code

--- sortListAlphabetically.py
+++ sortListAlphabetically.py
@@ -1,43 +1,30 @@
 #!/usr/bin/env python3
 
 import sys
+sys.setrecursionlimit(1500)
 
 words = sys.stdin.read()
-
-wordsList = []
-
 wordsList = words.split("\n")
-
-print(wordsList)
-
+#print(wordsList)
+wordsList.pop()
 def partition(low, high, arr):
-    pivot, part = ord(arr[high][0].lower()), low
+    pivot = arr[high].lower()
+    part = low - 1
     for j in range(low, high):
-        if ord(arr[j][0].lower()) < pivot:
-            arr[j], arr[part] = arr[part], arr[j]
+        if arr[j].lower() < pivot:
             part += 1
-        if ord(arr[j][0].lower()) == pivot:
-            char = 0
-            while ord(arr[j][char].lower()) == ord(arr[high][char].lower()):
-                if (char + 1) < len(arr[j]) and (char + 1) < len(arr[high]):
-                    if ord(arr[j][char + 1].lower()) < ord(arr[high][char + 1].lower()):
-                        arr[j], arr[part] = arr[part], arr[j]
-                        part += 1
-                    char += 1
-                else:
-                    break
-    arr[part], arr[high] = arr[high], arr[part]
-    return part
+            arr[j], arr[part] = arr[part], arr[j]
+    arr[part + 1], arr[high] = arr[high], arr[part + 1]
+    return part + 1
 
 def quicksort(low, high, arr):
     if len(arr) == 1:
         return arr
     if low < high:
-        pi = partition(low, high - 1, arr)
+        pi = partition(low, high, arr)
         quicksort(low, pi - 1, arr)
         quicksort(pi + 1, high, arr)
     return arr
-
+    
 for index in range(len(wordsList) - 1):
-    print(quicksort(0, len(wordsList) - 1, wordsList)[index])
-
+    print(quicksort(0, len(wordsList) - 1, wordsList)[index])                                                                                                            
