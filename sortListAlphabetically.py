b692f908651fbcf0d0c57b60738f25bf208b3235
Andrew Eldridge <andrew-eldridge@codermerlin.codermerlin.com>
2022-09-17 00:33:36 -0500

    OPTIMIZATION MF

--- sortListAlphabetically.py
+++ sortListAlphabetically.py
@@ -5,11 +5,10 @@ sys.setrecursionlimit(1500)
 
 words = sys.stdin.read()
 wordsList = words.split("\n")
-#print(wordsList)
 wordsList.pop()
+
 def partition(low, high, arr):
-    pivot = arr[high].lower()
-    part = low - 1
+    pivot, part = arr[high].lower(), low - 1
     for j in range(low, high):
         if arr[j].lower() < pivot:
             part += 1
@@ -18,13 +17,12 @@ def partition(low, high, arr):
     return part + 1
 
 def quicksort(low, high, arr):
-    if len(arr) == 1:
-        return arr
     if low < high:
         pi = partition(low, high, arr)
         quicksort(low, pi - 1, arr)
         quicksort(pi + 1, high, arr)
     return arr
     
+final = quicksort(0, len(wordsList) - 1, wordsList)
 for index in range(len(wordsList) - 1):
-    print(quicksort(0, len(wordsList) - 1, wordsList)[index])                                                                                                            
+    sys.stdout.write(final[index] + "\n")    
